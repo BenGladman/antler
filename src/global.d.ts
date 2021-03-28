2a0db3d939b84928524d51677791976f07a146f6
Ben Gladman <ben.gladman@gmail.com>
2021-03-28 14:58:07 +0100

    update deps and add deploy to gh-pages script

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,22 @@
+import type { ReactThreeFiber } from "react-three-fiber";
+import type { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import type { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import type { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+
 declare module "*.glb" {
   const path: string;
   export default path;
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      effectComposer: ReactThreeFiber.Node<
+        EffectComposer,
+        typeof EffectComposer
+      >;
+      renderPass: ReactThreeFiber.Node<RenderPass, typeof RenderPass>;
+      shaderPass: ReactThreeFiber.Node<ShaderPass, typeof ShaderPass>;
+    }
+  }
 }
